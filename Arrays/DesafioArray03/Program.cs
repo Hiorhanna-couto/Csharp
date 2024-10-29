3932e973578ba3c050f8625342cb675ea3696912
Hiorhanna-couto <hiorhannacouto2007@gmail.com>
2024-10-29 14:28:31 -0300

    os desafio de Array

---
+++ Arrays/DesafioArray03/Program.cs
@@ -0,0 +1,13 @@
+﻿for mes int range(1, 12):
+
+
+  temperatura = float(input(f"Digite a temperatura média do mês {mes}: "))
+  temperaturas.append(temperatura)
+
+# Encontra a maior e a menor temperatura
+maior_temperatura = max(temperaturas)
+menor_temperatura = min(temperaturas)
+
+# Imprime os resultados
+print(f"A maior temperatura do ano foi: {maior_temperatura:.1f}°C")
+print(f"A menor temperatura do ano foi: {menor_temperatura:.1f}°C")
