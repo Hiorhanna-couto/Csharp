3ae86f6d73260564a55487bc9daf1d74d314ef53
Hiorhanna-couto <hiorhannacouto2007@gmail.com>
2024-11-05 16:43:18 -0300

    Atividade Classes e Objetos

---
+++ POO/Calculator/Classes/Calculadora.cs
@@ -0,0 +1,18 @@
+
+namespace Calculator.Classes
+{
+    public class Calculadora
+    {
+        public int Numero1float;
+        public int Numero2float;
+
+        public string Somar = (+);
+
+        public string Subtrair = (-);
+
+        public string Multiplicar = (*);
+
+        public string Dividir =  (/);
+
+    }
+    }
